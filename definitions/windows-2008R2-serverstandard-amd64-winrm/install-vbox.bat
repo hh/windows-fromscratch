abf16f0d25d449f094a8285d63a5687094fc50de
Hippie Hacker <chris@hippiehacker.org>
2012-10-29 16:51:12 -0700

    restart windows after installing vbox windows additions

--- definitions/windows-2008R2-serverstandard-amd64-winrm/install-vbox.bat
+++ definitions/windows-2008R2-serverstandard-amd64-winrm/install-vbox.bat
@@ -2,3 +2,4 @@
 # http://downloads.sourceforge.net/sevenzip/7z920.exe
 cmd /c certutil -addstore -f "TrustedPublisher" a:oracle-cert.cer
 cmd /c e:\VBoxWindowsAdditions-amd64.exe /S
+cmd /c shutdown /r /t 0  /f /d p:4:1 /c "Restarting for VBoxWindowsAdditions"
